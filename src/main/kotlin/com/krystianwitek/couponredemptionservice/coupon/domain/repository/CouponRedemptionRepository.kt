f1be4bc1cd9feffa7e1ed3e05177673cc642567c
Krystian Witek <krystian.witek.it@gmail.com>
2026-08-28 10:24:02 +0200

    Implement coupon redemption repository (#11)
    * feat: implement coupon redemption repository
    
    * test: verify coupon redemption mappings
    
    * test: verify coupon redemption persistence
    
    * test: simplify coupon redemption persistence test
    
    ---------
    
    Co-authored-by: Krystian <krystian.d.witek@gmail.com>

--- src/main/kotlin/com/krystianwitek/couponredemptionservice/coupon/domain/repository/CouponRedemptionRepository.kt
+++ src/main/kotlin/com/krystianwitek/couponredemptionservice/coupon/domain/repository/CouponRedemptionRepository.kt
@@ -1,15 +1,23 @@
 package com.krystianwitek.couponredemptionservice.coupon.domain.repository
 
-import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.entity.CouponRedemption
+import com.krystianwitek.couponredemptionservice.coupon.domain.CouponRedemption
+import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.entity.CouponRedemption as CouponRedemptionEntity
+import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.mapper.toDomain
+import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.mapper.toEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
-interface CouponRedemptionRepository
+interface CouponRedemptionRepository {
+    fun save(couponRedemption: CouponRedemption): CouponRedemption
+}
 
 @Repository
 internal class CouponRedemptionRepositoryAdapter(
     private val jpaCouponRedemptionRepository: JpaCouponRedemptionRepository,
-) : CouponRedemptionRepository
+) : CouponRedemptionRepository {
+    override fun save(couponRedemption: CouponRedemption): CouponRedemption =
+        jpaCouponRedemptionRepository.save(couponRedemption.toEntity()).toDomain()
+}
 
-internal interface JpaCouponRedemptionRepository : JpaRepository<CouponRedemption, UUID>
+internal interface JpaCouponRedemptionRepository : JpaRepository<CouponRedemptionEntity, UUID>
