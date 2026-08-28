468c889659111a333bfd939371f6a59b7cf61b4a
Krystian <krystian.d.witek@gmail.com>
2026-08-28 10:09:48 +0200

    feat: implement coupon repository

--- src/main/kotlin/com/krystianwitek/couponredemptionservice/coupon/domain/repository/CouponRepository.kt
+++ src/main/kotlin/com/krystianwitek/couponredemptionservice/coupon/domain/repository/CouponRepository.kt
@@ -1,15 +1,29 @@
 package com.krystianwitek.couponredemptionservice.coupon.domain.repository
 
-import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.entity.Coupon
+import com.krystianwitek.couponredemptionservice.coupon.domain.Coupon
+import com.krystianwitek.couponredemptionservice.coupon.domain.CouponCode
+import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.entity.Coupon as CouponEntity
+import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.mapper.toDomain
+import com.krystianwitek.couponredemptionservice.coupon.infrastructure.persistence.mapper.toEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
-interface CouponRepository
+interface CouponRepository {
+    fun save(coupon: Coupon): Coupon
+
+    fun findByCode(code: CouponCode): Coupon?
+}
 
 @Repository
 internal class CouponRepositoryAdapter(
     private val jpaCouponRepository: JpaCouponRepository,
-) : CouponRepository
+) : CouponRepository {
+    override fun save(coupon: Coupon): Coupon = jpaCouponRepository.save(coupon.toEntity()).toDomain()
 
-internal interface JpaCouponRepository : JpaRepository<Coupon, UUID>
+    override fun findByCode(code: CouponCode): Coupon? = jpaCouponRepository.findByCode(code.value)?.toDomain()
+}
+
+internal interface JpaCouponRepository : JpaRepository<CouponEntity, UUID> {
+    fun findByCode(code: String): CouponEntity?
+}
