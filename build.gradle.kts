ffc9dfc9c7654a53d0e0333241dc4f6e76799835
Krystian <krystian.d.witek@gmail.com>
2026-08-28 15:19:08 +0200

    build: configure ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	alias(libs.plugins.kotlin.jvm)
 	alias(libs.plugins.kotlin.jpa)
 	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.ktlint)
 	alias(libs.plugins.spring.boot)
 	alias(libs.plugins.spring.dependency.management)
 }
