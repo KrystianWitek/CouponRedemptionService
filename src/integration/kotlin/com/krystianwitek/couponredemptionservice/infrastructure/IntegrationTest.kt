9823c6a6b9f28eed8c8509188d6e9eee67441cdb
Krystian <krystian.d.witek@gmail.com>
2026-08-29 15:45:04 +0200

    Configure HTTP integration testing

--- src/integration/kotlin/com/krystianwitek/couponredemptionservice/infrastructure/IntegrationTest.kt
+++ src/integration/kotlin/com/krystianwitek/couponredemptionservice/infrastructure/IntegrationTest.kt
@@ -1,11 +1,15 @@
 package com.krystianwitek.couponredemptionservice.infrastructure
 
 import com.krystianwitek.couponredemptionservice.infrastructure.config.PostgresTestConfiguration
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
 @Import(PostgresTestConfiguration::class)
+@ActiveProfiles("integration")
 internal annotation class IntegrationTest
