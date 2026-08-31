5772de55081c763f73710421e021f5b44699221d
Krystian <krystian.d.witek@gmail.com>
2026-08-31 22:45:16 +0200

    feat: return structured validation errors

--- src/main/kotlin/com/krystianwitek/couponredemptionservice/coupon/api/ErrorResponse.kt
+++ src/main/kotlin/com/krystianwitek/couponredemptionservice/coupon/api/ErrorResponse.kt
@@ -1,8 +1,12 @@
 package com.krystianwitek.couponredemptionservice.coupon.api
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class ErrorResponse(
     val errorCode: ErrorCode,
     val details: String,
+    val invalidFields: Set<String> = emptySet(),
 ) {
     enum class ErrorCode {
         COUPON_NOT_FOUND,
@@ -12,5 +16,6 @@ data class ErrorResponse(
         COUPON_ALREADY_REDEEMED,
         GEO_IP_LOOKUP_FAILED,
         INVALID_COUNTRY_CODE,
+        VALIDATION_ERROR,
     }
 }
